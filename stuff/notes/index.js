8c91e9475706f8652a1b03de9e3d30df71cf2551
leoribero <leo@ribero.net>
2022-05-18 15:34:22 +0200

    working on tuesday afternoon notes demo app from Ventu

---
+++ stuff/notes/index.js
@@ -0,0 +1,14 @@
+// Definir un CRUD
+
+function CreateNote(note) {
+    //
+    const { title, description } = note
+
+    
+
+
+
+    console.log(title, description)
+}
+
+CreateNote({title: 'nota de prueba 0', description: 'text content that define my very first note on this'})
---
+++ stuff/notes/utils/index.js
@@ -0,0 +1,2 @@
+
+const randomId = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
